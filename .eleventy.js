b6d7806ef2f9b825acfc53243a3d17071bf5a611
George Merlocco <george@merloc.co>
2020-02-16 17:33:36 -0500

    Migrate styles + assets from existing site

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/fonts')
   eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.addPassthroughCopy('src/audio')
 
   return {
     dir: {
